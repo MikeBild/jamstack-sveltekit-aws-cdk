8bcf4c099b86afa485c0119fd276d04444348fcb
Mike Bild <mike@mikebild.com>
2021-10-26 12:26:20 +0200

    more

--- infrastructure/deploy.js
+++ infrastructure/deploy.js
@@ -3,11 +3,13 @@ const { App } = require('@aws-cdk/core');
 const { CdkStackSveltekit } = require('@yippiecloud/cdk-stack-sveltekit');
 
 const app = new App();
+const name = process.env.STAGE || 'devspace';
+const domainName = process.env.DOMAIN || 'mikebild.com';
 
-new CdkStackSveltekit(app, 'JAMStack-Workshop', {	
+new CdkStackSveltekit(app, `JAMStack-Workshop-${name}`, {
+	hostName: name,
+	domainName,
 	serverPath: join(process.cwd(), '.svelte-kit/output/server-bundle'),
 	staticPath: join(process.cwd(), '.svelte-kit/output/static'),
-	domainName: 'mikebild.com',
-	hostName: 'demo1',	
 	env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
